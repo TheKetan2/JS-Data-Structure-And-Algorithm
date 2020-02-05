a03e7c6addab512d753eef504b60ac469ad3a859
Ketan <ketantoo@gmail.com>
2020-02-05 12:42:25 +0530

    Stack search element implementation

--- Stacks and Queues/Stack.js
+++ Stacks and Queues/Stack.js
@@ -23,4 +23,31 @@ Stack.prototype.pop = function() {
   return this.array.pop();
 };
 
+function stackAccessHthTopNode(stack1, n) {
+  let bufferArray = stack1.getBuffer();
+
+  if (n <= 0) throw "error";
+
+  let bufferStack = new Stack(bufferArray);
+
+  while (--n) {
+    bufferStack.pop();
+  }
+
+  return bufferStack.pop();
+}
+
+function stackSearch(stack1, element) {
+  var bufferArray = stack.getBuffer();
+
+  var bufferStack = new Stack(bufferArray);
+
+  while (!bufferStack.isEmpty()) {
+    if (bufferStack.pop() === element) {
+      return true;
+    }
+  }
+  return false;
+}
+
 let stack1 = new Stack();
