794635d722b0783d42b6e79819e8c3e35ba1a010
Ketan <ketantoo@gmail.com>
2020-02-05 12:35:21 +0530

    Stack implementation

--- Stacks and Queues/Stack.js
+++ Stacks and Queues/Stack.js
@@ -10,3 +10,17 @@ Stack.prototype.getBuffer = function() {
 Stack.prototype.isEmpty = function() {
   return this.array.length == 0;
 };
+
+Stack.prototype.peek = function() {
+  return this.array[this.array.length - 1];
+};
+
+Stack.prototype.puch = function(value) {
+  this.array.push(value);
+};
+
+Stack.prototype.pop = function() {
+  return this.array.pop();
+};
+
+let stack1 = new Stack();
