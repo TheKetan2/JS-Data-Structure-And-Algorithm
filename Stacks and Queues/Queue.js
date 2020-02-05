0a89fc198ae2d3555c56af75b9f1979cdedec62f
Ketan <ketantoo@gmail.com>
2020-02-05 12:54:21 +0530

    Queue access nth node element implementation

---
+++ Stacks and Queues/Queue.js
@@ -0,0 +1,37 @@
+function Queue(array) {
+  this.array = [];
+  if (array) this.array = array;
+}
+
+Queue.prototype.getBuffer = function() {
+  return this.array.slice();
+};
+
+Queue.prototype.isEmpty = function() {
+  return this.array.length === 0;
+};
+
+Queue.prototype.peek = function() {
+  return this.array[0];
+};
+
+Queue.prototype.enqueue = function(value) {
+  return this.array.push(value);
+};
+
+Queue.prototype.dequeue = function() {
+  return this.array.shift();
+};
+
+function queueAccessNthTopNode(queue, n) {
+  var bufferArray = queue.getBuffer();
+  if (n <= 0) {
+    throw "error";
+  }
+  var bufferQueue = new Queue(bufferArray);
+
+  while (--n) {
+    bufferQueue.dequeue();
+  }
+  return bufferQueue.dequeue();
+}
